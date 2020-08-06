dfe6a5cef91deedc718a5d3d1b5d72cf3476da48
foreveryc <1209957987@qq.com>
2020-08-06 19:25:01 +0800

    genenersql

---
+++ src/com/ytao/mapper/TbOrderMapper.java
@@ -0,0 +1,22 @@
+package com.ytao.mapper;
+
+import com.ytao.pojo.TbOrder;
+import com.ytao.pojo.TbOrderExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbOrderMapper {
+    int countByExample(TbOrderExample example);
+
+    int deleteByExample(TbOrderExample example);
+
+    int insert(TbOrder record);
+
+    int insertSelective(TbOrder record);
+
+    List<TbOrder> selectByExample(TbOrderExample example);
+
+    int updateByExampleSelective(@Param("record") TbOrder record, @Param("example") TbOrderExample example);
+
+    int updateByExample(@Param("record") TbOrder record, @Param("example") TbOrderExample example);
+}
